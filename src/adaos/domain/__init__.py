cc2c8f95721c9225cfd218ab5bd713afcf845933
ShvedOG <lllv3d@yandex.ru>
2026-05-15 18:18:08 +0300

    Добавлена основа ABI событий

--- src/adaos/domain/__init__.py
+++ src/adaos/domain/__init__.py
@@ -1,5 +1,16 @@
 from .types import SkillId, ScenarioId, Event, ProcessSpec
+from .event_envelope import EventEnvelope, enrich_event_payload, normalize_event_envelope
 from .skill import SkillMeta
 from .skill_registry import SkillRecord
 
-__all__ = ["SkillId", "ScenarioId", "Event", "ProcessSpec", "SkillMeta", "SkillRecord"]
+__all__ = [
+    "SkillId",
+    "ScenarioId",
+    "Event",
+    "EventEnvelope",
+    "ProcessSpec",
+    "SkillMeta",
+    "SkillRecord",
+    "enrich_event_payload",
+    "normalize_event_envelope",
+]
